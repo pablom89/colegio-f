21f0c937c00099efae417158563481d2b84e9360
pablom89 <59423990+pablom89@users.noreply.github.com>
2021-08-29 13:12:48 -0300

    add file via upload
    modificando registrados.svelte

--- src/vistas/usuariosregistrados.svelte
+++ src/vistas/usuariosregistrados.svelte
@@ -54,6 +54,29 @@
     }
   };
 
+  function tabla(info) {
+    contenido.innerHTML = "";
+    for (let valor of info) {
+      var fecha = new Date(valor.fechanac);
+      var fechalocale = fecha.toLocaleDateString();
+      var urlfoto = valor.fotodni.url;
+      console.log(urlfoto);
+      contenido.innerHTML += `
+                <tr style="border: 1px solid black; border-collapse: collapse;">
+                    <td style="border: 1px solid black; border-collapse: collapse;">${valor.id}</td>
+                    <td style="border: 1px solid black; border-collapse: collapse;">${valor.nombre}</td> 
+                    <td style="border: 1px solid black; border-collapse: collapse;">${valor.apellido}</td> 
+                    <td style="border: 1px solid black; border-collapse: collapse;">${valor.email}</td>
+                    <td style="border: 1px solid black; border-collapse: collapse;">${fechalocale}</td>
+                    <td style="border: 1px solid black; border-collapse: collapse;">${valor.sexo}</td>
+                    <td style="border: 1px solid black; border-collapse: collapse;">${valor.nacionalidad}</td>
+                    <td style="border: 1px solid black; border-collapse: collapse;"><img src=${urlfoto} style="width: 33px"></td>
+                    <td style="border: 1px solid black; border-collapse: collapse;"><button on:click={confirmar(${valor.id})}>Confirmar</button></td>     
+                </tr>
+                `;
+    }
+  }
+
   onMount(async () => {
     const respuesta = await fetch(
       "https://colegio-b.herokuapp.com/Usuarios?verificado=false",
@@ -69,29 +92,6 @@
         console.log(info);
         tabla(info);
       });
-
-    function tabla(info) {
-      contenido.innerHTML = "";
-      for (let valor of info) {
-        var fecha = new Date(valor.fechanac);
-        var fechalocale = fecha.toLocaleDateString();
-        var urlfoto = valor.fotodni.url;
-        console.log(urlfoto);
-        contenido.innerHTML += `
-                <tr style="border: 1px solid black; border-collapse: collapse;">
-                    <td style="border: 1px solid black; border-collapse: collapse;">${valor.id}</td>
-                    <td style="border: 1px solid black; border-collapse: collapse;">${valor.nombre}</td> 
-                    <td style="border: 1px solid black; border-collapse: collapse;">${valor.apellido}</td> 
-                    <td style="border: 1px solid black; border-collapse: collapse;">${valor.email}</td>
-                    <td style="border: 1px solid black; border-collapse: collapse;">${fechalocale}</td>
-                    <td style="border: 1px solid black; border-collapse: collapse;">${valor.sexo}</td>
-                    <td style="border: 1px solid black; border-collapse: collapse;">${valor.nacionalidad}</td>
-                    <td style="border: 1px solid black; border-collapse: collapse;"><img src=${urlfoto} style="width: 33px"></td>
-                    <td style="border: 1px solid black; border-collapse: collapse;"><button onclick='confirmar(${valor.id})'>Confirmar</button></td>     
-                </tr>
-                `;
-      }
-    }
   });
 </script>
 
